fe0ec88e11e39479dc4f6639d92cefa124da0363
fayecai0322 <156372600+fayecai0322@users.noreply.github.com>
2025-03-28 15:47:57 -0700

    enrollment debug

--- Enrollments/dao.js
+++ Enrollments/dao.js
@@ -3,10 +3,16 @@ import {v4 as uuidv4} from "uuid";
 
 //Takes the current user ID and the newly created course ID
 //Saves the association to the Database.enrollments table
-export function enrollUserInCourse(userId, courseId){
-    const {enrollments} = Database;
-    enrollments.push({_id:uuidv4(), user:userId, course:courseId});
-}
+export function enrollUserInCourse(userId, courseId) {
+    const { enrollments } = Database;
+    enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
+  }
+  
+  export function unenrollUserFromCourse(userId, courseId) {
+    const { enrollments } = Database;
+    const index = enrollments.findIndex(e => e.user === userId && e.course === courseId);
+    if (index !== -1) enrollments.splice(index, 1);
+  }
 export function findEnrollmentsByUser(userId) {
     return Database.enrollments.filter(e => e.user === userId);
   }
